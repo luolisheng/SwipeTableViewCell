035d0fdf6ed0892a194aca3d3a06413e76fcc7ba
luolisheng <ilisheng.luo@gmail.com>
2013-10-17 23:31:44 +0800

    first

---
+++ SwipeTableViewCellController.h
@@ -0,0 +1,13 @@
+//
+//  SwipeTableViewCellController.h
+//  SwipeTableViewCell
+//
+//  Created by luolisheng on 13-10-17.
+//  Copyright (c) 2013年 luolisheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SwipeTableViewCellController : UITableViewController
+
+@end
